fb48411204c258bd1c58ac41a0a4e7b2469a2dbf
Leo Coogan <leocoogan@tutanota.com>
2021-09-19 21:24:30 -0400

    Removed test script

--- src/testi.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env dash
-find ./data/*/*.xml.lz -type f \( -name "*game*" -o -name "*demo*" -o -name "*chapter*" \) |
-	while read file; do ./scripts/nlzss_cli.py -d -i "$file" -o "${file%.*}"; done
